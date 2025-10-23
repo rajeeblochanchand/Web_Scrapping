8135f98f55cee531a9a4f5e514981449d9b688f8
Rajeeb Lochan Chand <166954079+rajeeblochanchand@users.noreply.github.com>
2025-10-23 16:45:46 +0530

    complete scrapping

--- scrapper.py
+++ scrapper.py
@@ -3,18 +3,28 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 base_url = 'https://quotes.toscrape.com/'
-response = requests.get(base_url)
-#print(response.text)
-soup = BeautifulSoup(response.text, 'lxml')
-quotes = soup.find_all('div', class_='quote')
-for quote in quotes:
-    quote_text = quote.find('span', class_='text').text
-    author_name = quote.find('small', class_='author').text
-    print(f'Quote: {quote_text}\nAuthor: {author_name}')
-    print('\n')
+page_count = 1
 
-next_button = soup.find('li', class_='next')
-if next_button:
-    next_a = next_button.find('a')
-    relative_url = next_a['href']
-    next_page_url = urljoin(base_url, relative_url)
+current_page_url = base_url
+while(current_page_url):
+    response = requests.get(current_page_url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    quotes = soup.find_all('div', class_='quote')
+    print(f'--- Page {page_count} ---')
+    for quote in quotes:
+        quote_text = quote.find('span', class_='text').text
+        author_name = quote.find('small', class_='author').text
+        print(f'Quote: {quote_text}\nAuthor: {author_name}')
+        print('\n')
+
+    next_button = soup.find('li', class_='next')
+    if next_button:
+        page_count += 1
+        next_a = next_button.find('a')
+        relative_url = next_a['href']
+        next_page_url = urljoin(base_url, relative_url)
+        current_page_url = next_page_url
+    else:
+        current_page_url = None
+
+
